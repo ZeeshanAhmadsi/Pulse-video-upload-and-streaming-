9249b2dfdca1e33f3eb1fc9be81f633765e862ea
ZeeshanAhmadsi <155843561+ZeeshanAhmadsi@users.noreply.github.com>
2025-12-19 20:11:55 +0530

    commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -68,7 +68,6 @@ const corsOptions = {
 
 // Middleware
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
