bcda48afb0108f1ef1827f3fa0b626064daa8a07
ZeeshanAhmadsi <155843561+ZeeshanAhmadsi@users.noreply.github.com>
2025-12-19 19:33:57 +0530

    stagging changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -71,6 +71,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.options('*', cors(corsOptions));
+
 
 // Routes
 app.use('/api/auth', authRoutes);
